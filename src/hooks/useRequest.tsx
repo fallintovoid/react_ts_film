92dbcdd29151539dfb7618bba52a78ed0ded3e95
Illya <sypersys228@gmail.com>
2022-07-03 16:22:27 +0200

    request added

---
+++ src/hooks/useRequest.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useState } from 'react'
+
+type Props = {}
+
+const useRequest = (props: Props) => {
+    const [error, setError] = useState(false)
+    const [loading, setLoading] = useState(true)
+    const request = async (url: string) => {
+        try {
+            const res = await fetch(url);
+            if (!res.ok) {
+                setLoading(false)
+                throw new Error("Couldn`t fetch status:" + res.status);
+            }
+            const data = await res.json()
+            setLoading(false)
+            return data;
+        } catch (error) {
+            setError(true);
+            setLoading(false);
+            throw error;
+        }
+    }
+    return {request, loading, error}
+}
+
+export default useRequest
