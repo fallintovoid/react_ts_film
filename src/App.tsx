7ad65d26ce28d280d41810df3094d737ba391b03
unknown <avicostov2@gmail.com>
2022-07-04 00:28:48 +0200

    added requests

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,17 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import { MovieCard } from './pages/MovieCard/MovieCard';
 import Home from './pages/Home/Home';
 
-import './styles/App.scss'
-
 
 const App: React.FC = () => {
+  console.log('render app');
+
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Navigate to='/home' replace={true} />} />
         <Route path='/home' element={<Home />} />
         <Route path='/movie' element={<MovieCard />} />
+        <Route path='/notexist' element={<h1></h1>} />
       </Routes>
     </div>
   );
--- src/pages/MovieCard/MovieCard.tsx
+++ src/pages/MovieCard/MovieCard.tsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getMovie } from "../../api/api";
 
 import './MovieCard.scss';
 
 export const MovieCard: React.FC = React.memo(() => {
+    const [movie, setMovie] = useState<Movie | null>(null);
+
+    const getMovieFromServ = async () => {
+        setMovie(await getMovie('dd'));
+    }
+
+    useEffect(() => {
+        getMovieFromServ();
+    }, []);
+
+    console.log('render movie');
+
     return (
         <div className="container">
             <div className="movie">
                 <div className="movie__img"></div>
                 <div className="movie__body">
                     <div className="movie__body--title">
-                        TITLE
+                        {movie?.Title}
                     </div>
                     <div className="movie__body--description">
-                        This film about everything
+                        {movie?.Plot}
                     </div>
                 </div>
             </div>
