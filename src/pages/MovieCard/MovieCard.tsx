a232dba9346d1247c286efd144d170840dcae840
unknown <avicostov2@gmail.com>
2022-07-03 22:44:16 +0200

    added movie and router

---
+++ src/pages/MovieCard/MovieCard.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import './MovieCard.scss';
+
+export const MovieCard: React.FC = React.memo(() => {
+    return (
+        <div className="movie">
+            <div className="movie__img"></div>
+            <div className="movie__body">
+                <div className="movie__body--title">
+                    TITLE
+                </div>
+                <div className="movie__body--description">
+                    This film about everything
+                </div>
+            </div>
+        </div>
+    )
+});
